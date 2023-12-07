7a870397f1ad9debc9528760f10f88366299c459
Janishar Ali <janishar.ali@gmail.com>
2023-12-07 16:06:45 +0530

    fix template

--- .templates/route/index.ts
+++ .templates/route/index.ts
@@ -1,15 +1,15 @@
 /*
 import express from 'express';
 import { ProtectedRequest } from 'app-request';
-import { SuccessResponse } from '../../../core/ApiResponse';
-import asyncHandler from '../../../helpers/asyncHandler';
-import validator, { ValidationSource } from '../../../helpers/validator';
+import { SuccessResponse } from '../../core/ApiResponse';
+import asyncHandler from '../../helpers/asyncHandler';
+import validator, { ValidationSource } from '../../helpers/validator';
 import schema from './schema';
-import { BadRequestError } from '../../../core/ApiError';
-import role from '../../../helpers/role';
-import authentication from '../../../auth/authentication';
-import authorization from '../../../auth/authorization';
-import { RoleCode } from '../../../database/model/Role';
+import { BadRequestError } from '../../core/ApiError';
+import role from '../../helpers/role';
+import authentication from '../../auth/authentication';
+import authorization from '../../auth/authorization';
+import { RoleCode } from '../../database/model/Role';
 
 const router = express.Router();
 
