656c6f7e1f875a913fb90418b74074c1f57c71f5
Janishar Ali <janishar.ali@gmail.com>
2023-12-08 14:09:56 +0530

    create content blank route

---
+++ src/routes/content/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { ProtectedRequest } from 'app-request';
+import { SuccessResponse } from '../../core/ApiResponse';
+import asyncHandler from '../../helpers/asyncHandler';
+import validator, { ValidationSource } from '../../helpers/validator';
+import schema from './schema';
+import role from '../../helpers/role';
+import authentication from '../../auth/authentication';
+import authorization from '../../auth/authorization';
+import { RoleCode } from '../../database/model/Role';
+
+const router = express.Router();
+
+/*----------------------------------------------------------------*/
+router.use(authentication, role(RoleCode.VIEWER), authorization);
+/*----------------------------------------------------------------*/
+
+router.post(
+  '/sample',
+  validator(schema.sample, ValidationSource.BODY),
+  asyncHandler(async (req: ProtectedRequest, res) => {
+    new SuccessResponse('Success', {}).send(res);
+  }),
+);
+
+export default router;
